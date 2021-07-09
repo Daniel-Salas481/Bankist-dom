28a276b19a75e05dbba557e6e0def9c78fdbfd1c
Daniel-Salas481 <dsalas6@asu.edu>
2021-07-08 18:11:23 -0700

    Finished event delegation

--- script.js
+++ script.js
@@ -30,98 +30,179 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-//Selecting and Deleting Elements
+// //Selecting and Deleting Elements
 
-//Selecting an element from the page
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// //Selecting an element from the page
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-//returns the first element that has header
-const header = document.querySelector(".header");
-//selects all elements with 'section'
-const allSections = document.querySelectorAll(".section");
-console.log(allSections);
+// //returns the first element that has header
+// const header = document.querySelector(".header");
+// //selects all elements with 'section'
+// const allSections = document.querySelectorAll(".section");
+// console.log(allSections);
 
-document.getElementById("section--1");
+// document.getElementById("section--1");
 
-//Returns an HTML collection
-const allButtons = document.getElementsByTagName("button");
-console.log(allButtons);
+// //Returns an HTML collection
+// const allButtons = document.getElementsByTagName("button");
+// console.log(allButtons);
 
-//returns a live HTML collection
-console.log(document.getElementsByClassName("btn"));
+// //returns a live HTML collection
+// console.log(document.getElementsByClassName("btn"));
 
-//Creating and inserting elements
-const message = document.createElement("div");
-message.classList.add("cookie-message");
-//message.textContent = "We use cookies for improved functionalty and analytics";
-message.innerHTML =
-  'We use cookies for improved functionalty and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// //Creating and inserting elements
+// const message = document.createElement("div");
+// message.classList.add("cookie-message");
+// //message.textContent = "We use cookies for improved functionalty and analytics";
+// message.innerHTML =
+//   'We use cookies for improved functionalty and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-//We can use the prepend and append to not only insert element but also move it
-//header.prepend(message);
-header.append(message);
-//header.append(message.cloneNode(true));
+// //We can use the prepend and append to not only insert element but also move it
+// //header.prepend(message);
+// header.append(message);
+// //header.append(message.cloneNode(true));
 
-//header.before(message);
-//header.after(message);
+// //header.before(message);
+// //header.after(message);
 
-//Delete elements
-//This will remove the cookie message on the bottom
-//Using the .remove();
-document
-  .querySelector(".btn--close-cookie")
-  .addEventListener("click", function () {
-    message.remove();
+// //Delete elements
+// //This will remove the cookie message on the bottom
+// //Using the .remove();
+// document
+//   .querySelector(".btn--close-cookie")
+//   .addEventListener("click", function () {
+//     message.remove();
+//   });
+
+// //Styles
+// message.style.backgroundColor = "#37383d";
+// message.style.width = "120%";
+
+// console.log(message.style.height);
+// console.log(message.style.backgroundColor);
+
+// //contains all of the properties of the value
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// //changes the height of the message
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+// //Easily change the stlye of the page
+// document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// //Attributes
+// const logo = document.querySelector(".nav__logo");
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
+
+// logo.alt = "Beautiful minimalist logo";
+
+// //Non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute("designer"));
+// logo.setAttribute("company", "Bankist");
+
+// //the absolute URL of the image
+// console.log(logo.src);
+// //the literal URL of the image
+// console.log(logo.getAttribute("src"));
+
+// //link attributes
+// const link = document.querySelector(".twitter-link");
+// console.log(link.href);
+// console.log(link.getAttribute("href"));
+
+// //Classs
+// logo.classList.add("c");
+// logo.classList.remove("c");
+// logo.classList.toggle("c");
+// logo.classList.contains("c"); //not includes
+
+// // Don't use
+// // logo.className = "daniel";
+
+//Smooth Scrolling
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+//getBoundingClientRect method returns a DOMRect object
+// const s1coords = section1.getBoundingClientRect();
+// console.log(s1coords);
+
+// console.log(e.target.getBoundingClientRect());
+
+// console.log("current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+//Scrolling
+// window.scrollTo(
+//   s1coords.left + window.pageXOffset,
+//   s1coords.top + window.pageYOffset
+// );
+
+//old-school way
+// window.scrollTo({
+//   left: s1coords.left + window.pageXOffset,
+//   top: s1coords.top + window.pageYOffset,
+//   behavior: "smooth",
+// });
+
+btnScrollTo.addEventListener("click", function (e) {
+  section1.scrollIntoView({
+    behavior: "smooth",
   });
+});
 
-//Styles
-message.style.backgroundColor = "#37383d";
-message.style.width = "120%";
+//event listener
 
-console.log(message.style.height);
-console.log(message.style.backgroundColor);
+// const h1 = document.querySelector("h1");
 
-//contains all of the properties of the value
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// const alertH1 = function (e) {
+//   alert("addEventListner: Great! you are reading the heading!");
 
-//changes the height of the message
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+//   //we remove the event listener here
+//   h1.removeEventListener("mouseenter", alertH1);
+// };
 
-//Easily change the stlye of the page
-document.documentElement.style.setProperty("--color-primary", "orangered");
+//mouseenter is an event wherever the mouse is hovered over
+// h1.addEventListener("mouseenter", alertH1);
 
-//Attributes
-const logo = document.querySelector(".nav__logo");
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// //remove events by using the setTimeout
+// setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
 
-logo.alt = "Beautiful minimalist logo";
+//This is more of an old school way
+// h1.onmouseenter = function (e) {
+//   alert("addEventListner: Great! you are reading the heading!");
+// };
 
-//Non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute("designer"));
-logo.setAttribute("company", "Bankist");
+// //EVENT PROPAGATION
+// // rgb(255,255,255)
 
-//the absolute URL of the image
-console.log(logo.src);
-//the literal URL of the image
-console.log(logo.getAttribute("src"));
+// //The event happens in the document root
+// //and then it goes to the target
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
 
-//link attributes
-const link = document.querySelector(".twitter-link");
-console.log(link.href);
-console.log(link.getAttribute("href"));
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 
-//Classs
-logo.classList.add("c");
-logo.classList.remove("c");
-logo.classList.toggle("c");
-logo.classList.contains("c"); //not includes
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("LINK", e.target, e.currentTarget);
 
-// Don't use
-// logo.className = "daniel";
+//   //Stop the propagation
+//   //e.stopPropagation();
+// });
+
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("CONTAINER", e.target, e.currentTarget);
+// });
+
+// document.querySelector(".nav").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("NAV", e.target, e.currentTarget);
+// });
