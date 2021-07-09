7656f3e87e3de1ff94fc65d422de3306f5159a14
Daniel-Salas481 <dsalas6@asu.edu>
2021-07-08 19:02:52 -0700

    experimented with traversing DOM

--- script.js
+++ script.js
@@ -101,6 +101,10 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
+///////////////////////////////
+///Experimenting DOM and Events
+///////////////////////////////
+
 // //Selecting and Deleting Elements
 
 // //Selecting an element from the page
@@ -246,4 +250,37 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 // document.querySelector(".nav").addEventListener("click", function (e) {
 //   this.style.backgroundColor = randomColor();
 //   console.log("NAV", e.target, e.currentTarget);
+// });
+
+// //TRAVERSING DOM
+// const h1 = document.querySelector("h1");
+
+// //Going downwards: child
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.childNodes);
+// //gives us an HTML collection
+// console.log(h1.children);
+// //sets the first child to white
+// h1.firstElementChild.style.color = "white";
+// h1.lastElementChild.style.color = "pink";
+
+// //Going upwards: parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// //returns the closet element
+// h1.closest(".header").style.background = "var(--gradient-secondary)";
+// h1.closest(".header").style.background = "var(--gradient-primary)";
+
+// //Going sideways: siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// //gets all of the siblings
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = "scale(0.5)";
 // });
